c329bc04f793b2df35bf0f33cc810d577c2a2efd
procrastinatorMuffin <danil.sholudko@icloud.com>
2024-04-18 15:57:35 +0200

    add: support of env variables in bench script

--- db/migrations/db_bench.py
+++ db/migrations/db_bench.py
@@ -4,14 +4,19 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 import time
 import statistics
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv(override=True)
 
 def connect_db():
     return psycopg2.connect(
-        dbname="cogitotest", 
-        user="postgres", # Replace this with your PostgreSQL username
-        password="pass", # Replace this with your PostgreSQL password
-        host="localhost", 
-        port="5432"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
 
 def db_operation(cursor, operation_type):
