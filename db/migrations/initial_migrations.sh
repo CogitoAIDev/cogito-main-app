14ac8004c004f6eb6425efe8a13ef4bfdd224304
procrastinatorMuffin <danil.sholudko@icloud.com>
2024-04-18 15:57:23 +0200

    add: support of env variables in migration script

--- db/migrations/initial_migrations.sh
+++ db/migrations/initial_migrations.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Set database parameters
-DB_USER="postgres" # Replace this with your PostgreSQL username
-DB_PASSWORD="pass" # Replace this with your PostgreSQL password
-DB_HOST="localhost"
-DB_PORT="5432"
+# Load environment variables
+set -o allexport
+source ../.env
+set +o allexport
 
-NEW_DB_NAME="cogitotest"
+NEW_DB_NAME="$DB_NAME"
 
 SQL_SCRIPT_PATH="001_initial_schema.sql"
 
 export PGPASSWORD=$DB_PASSWORD
 
+# Create a new database
 psql -h $DB_HOST -U $DB_USER -d postgres -p $DB_PORT -c "CREATE DATABASE $NEW_DB_NAME;"
 if [ $? -eq 0 ]
 then
@@ -21,7 +21,7 @@ else
     exit 1
 fi
 
-
+# Run the migration SQL script to setup tables and relationships
 psql -h $DB_HOST -U $DB_USER -d $NEW_DB_NAME -p $DB_PORT -f $SQL_SCRIPT_PATH
 if [ $? -eq 0 ]
 then
@@ -31,3 +31,4 @@ else
 fi
 
 unset PGPASSWORD
+unset NEW_DB_NAME
